ac1a15753f10a63f1a567180dec135ba6e983a7e
MahdiT54 <mahditanzim2@gmail.com>
2024-06-06 09:31:17 -0400

    hero

--- balanceai-frontend/components/Hero.js
+++ balanceai-frontend/components/Hero.js
@@ -1,9 +1,11 @@
-import herostyles from '../styles/Hero.module.css';
+import herostyles from "../styles/Hero.module.css";
 
 export default function Hero() {
   return (
-    <div className={herostyles.hero__frame}>
-      
+    <div className={herostyles.hero}>
+      <div className={herostyles.hero__frame}>
+        <h1 className={herostyles["hero__frame--h1"]}>Cash Flow Made Easy for Creators</h1>
+      </div>
     </div>
   );
 }
