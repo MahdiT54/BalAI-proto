a17534e6486b42d7eb89bfe88d2fd7ca5f605a0e
MahdiT54 <mahditanzim2@gmail.com>
2024-06-06 08:28:58 -0400

    1

---
+++ balanceai-frontend/components/Hero.js
@@ -0,0 +1,9 @@
+import herostyles from '../styles/Hero.module.css';
+
+export default function Hero() {
+  return (
+    <div className={herostyles.hero__frame}>
+      
+    </div>
+  );
+}
---
+++ balanceai-frontend/components/Navbar.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import logo from "../public/Logo.svg";
+import navstyles from "../styles/Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={navstyles.nav}>
+      <Link href="/">
+        <figure className={navstyles.nav__logo}>
+          <img src="/Logo.svg" className={navstyles["nav__logo--img"]} alt="" />
+          <h6 className={navstyles["nav__logo--text"]}>BalanceAI</h6>
+        </figure>
+      </Link>
+      <ul className={navstyles.nav__list}>
+        <Link href="/">
+          <li
+            className={`${navstyles.nav__link} ${navstyles["nav__link--home"]}`}
+          >
+            Home
+          </li>
+        </Link>
+        <Link href="/about">
+          <li className={navstyles.nav__link}>About</li>
+        </Link>
+        <Link href="/contact">
+          <li className={navstyles.nav__link}>Contact</li>
+        </Link>
+        <Link href="/technology">
+          <li className={navstyles.nav__link}>Technology</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
